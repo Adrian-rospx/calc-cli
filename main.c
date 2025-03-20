c7356149bafe6a8bbc358d40bdc95075715b9627
Adrian-isp <adrianrosu5400@gmail.com>
2025-03-20 23:25:03 +0200

    basic tokenizer

--- main.c
+++ main.c
@@ -3,13 +3,16 @@
 #include <stdlib.h>
 
 void removeWhitespace(char* text);
+void tokenize(char mat[20][10], char *text);
 
 int main() {
     char* input = (char*)malloc(100 * sizeof(char));
+    char mat[20][10];
 
     fgets(input, 100, stdin);
 
     removeWhitespace(input);
+    tokenize(mat, input);
 
     printf("%s\n", input);
 
@@ -32,4 +35,22 @@ void removeWhitespace(char *text) {
         }
     }
     *dest = '\0';
+}
+
+// split string into tokens
+void tokenize(char mat[20][10], char *text) {
+    int i = 0;
+
+    for(char* ptr = text; *ptr != '\0'; ptr++) {
+        if(strchr("+-*/", *ptr)) {
+            mat[i][0] = *ptr;
+            i++;
+        }
+
+        if('0'<=*ptr && *ptr<='9') {
+            mat[i][0] = *ptr;
+            i++;
+        }
+        
+    }
 }

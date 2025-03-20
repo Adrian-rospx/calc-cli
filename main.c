9483b5f8bbdf6acc1a807c2c658ef4c090c189e0
Adrian-isp <adrianrosu5400@gmail.com>
2025-03-20 23:47:52 +0200

    tokenizer working, unideal

--- main.c
+++ main.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 void removeWhitespace(char* text);
 void tokenize(char mat[20][10], char *text);
 
 int main() {
     char* input = (char*)malloc(100 * sizeof(char));
-    char mat[20][10];
+    char tokens[20][10];
 
     fgets(input, 100, stdin);
 
     removeWhitespace(input);
-    tokenize(mat, input);
+    tokenize(tokens, input);
 
     printf("%s\n", input);
+
+    for(int i = 0; tokens[i][0] != '\0'; i++) {
+        printf("%s\n", tokens[i]);
+    }
 
     free(input);
     return 0;
@@ -39,18 +44,27 @@ void removeWhitespace(char *text) {
 
 // split string into tokens
 void tokenize(char mat[20][10], char *text) {
-    int i = 0;
+    int i = 0; // Token index
+    int j = 0; // character index in a token
 
     for(char* ptr = text; *ptr != '\0'; ptr++) {
-        if(strchr("+-*/", *ptr)) {
-            mat[i][0] = *ptr;
-            i++;
-        }
 
-        if('0'<=*ptr && *ptr<='9') {
+        if(isdigit(*ptr)) {
+            j = 0;  // store multi digit numbers
+            while(isdigit(*ptr)){
+                mat[i][j] = *ptr; 
+                j++;
+                ptr++;
+            }
+            mat[i][j] = '\0';   // nullifly
+            i++;    // move to next token
+            ptr--;  // account for extra increment
+        }
+        // handle operators:
+        else if(strchr("+-*/()", *ptr)) {
             mat[i][0] = *ptr;
+            mat[i][1] = '\0';
             i++;
         }
-        
     }
 }
